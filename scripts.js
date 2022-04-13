628b80176b7580304b8fe668667703e7771ea353
JustRandomNoob <72201398+JustRandomNoob@users.noreply.github.com>
2022-04-13 22:26:24 +0530

    Final commit

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
 function MyFunction() {
 
     // document.getElementById("line2").innerHTML = "Player was not found in skyblock island";
-    document.getElementById("line").innerHTML = "Updating in 5 minutes";
+    document.getElementById("line").innerHTML = "Tracking Started Updating in 30 seconds!";
 
     fetch("https://api.hypixel.net/status?uuid=71e75e00-580d-4f2e-b026-5f6785dceca2&key=e1084c05-ee59-4bc1-926b-abb936702cc5")
         .then(result => result.json())
@@ -18,24 +18,19 @@ function MyFunction() {
 
                         const GameType = status.session.mode;
                         if (!status.session.online == 'true') {
-                           console.log("Player is offline");
-                           
+                            Push.create('Player is offline!');
+
                         } else if (GameType == 'dynamic') {
 
-                            console.log(`Player is online`);
                             document.getElementById("line").innerHTML = "Player is Online";
                             document.getElementById("line2").innerHTML = `Gamemode: ${GameType}`;
-                        } else console.log("Player was kicked");
+                        } else Push.create('Player was kicked!')
+
+
                     })
-            }, 15000); // use 60000 for 1 minute 
+            }, 60000); // use 60000 for 1 minute 
 
         })
 
 }
 
-
-function PushNotif() {
-
-
-    Push.create('Hello World!')
-}
