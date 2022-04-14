aa37847392ea5cf6603208bbb2ce29e7f4b6755b
JustRandomNoob <72201398+JustRandomNoob@users.noreply.github.com>
2022-04-14 11:27:35 +0530

    fix

--- scripts.js
+++ scripts.js
@@ -2,7 +2,6 @@
 function MyFunction() {
 
     // document.getElementById("line2").innerHTML = "Player was not found in skyblock island";
-    document.getElementById("run1").innerHTML = "Website is running";
 
     fetch("https://api.hypixel.net/status?uuid=71e75e00-580d-4f2e-b026-5f6785dceca2&key=e1084c05-ee59-4bc1-926b-abb936702cc5")
         .then(result => result.json())
